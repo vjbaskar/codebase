d7b879dd4b0d6b5b30cb82142fb479598d52dbc8
Vijaya Baskar <34027254+vjbaskar@users.noreply.github.com>
2026-01-15 13:01:51 +0000

    support for sys

--- multiome_predictor/dataset.py
+++ multiome_predictor/dataset.py
@@ -4,7 +4,7 @@ import json
 import os
 from dataclasses import dataclass
 from typing import Optional, Tuple, List
-
+import sys
 import numpy as np
 import scipy.sparse as sp
 from sklearn.decomposition import TruncatedSVD
--- multiome_predictor/train.py
+++ multiome_predictor/train.py
@@ -3,6 +3,7 @@ from __future__ import annotations
 import argparse
 import json
 import os
+import sys
 from typing import List, Optional
 
 import numpy as np
